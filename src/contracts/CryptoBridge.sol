0977d08593228cec7f94d6ca257ceb7b60e11975
Federico Candiolo <candiolof@gmail.com>
2024-01-14 01:31:57 -0300

    Complete contract with testing. No front-end

--- src/contracts/CryptoBridge.sol
+++ src/contracts/CryptoBridge.sol
@@ -1,116 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-
 contract CryptoBridge {
-
-    string public name = "CryptoBridge";
-
-    //Stores FundRaising
-    uint public fundRaisingsCount = 0;
-    mapping(uint => string) public fundraisingsList;
-    mapping(string => FundRaising) private fundraisings;
-    mapping(string => mapping(uint=>Donation)) public fundraisingsdonations; //Double mapping to get each donation of any fundraising
-    mapping(string => bool) private fundraisingExist;
-
-    struct FundRaising {
-        uint id;
-        string string_id;
-        string imghash;
-        uint totalAmount;
-        uint amountToRetrieve;
-        uint donorCount;
-        bool isOpen;
-        address payable author;
-    }
-
-    event FundRaisingModified (
-        FundRaising fundraising
-    );
-    
-    struct FundRaisingDonations {
-        mapping(uint=>Donation) donations;
-    }
-
-    struct Donation {
-        uint amount;
-        string message;
-    }
-
-    //Creates FundRaising
-    function createFundRaising(string memory _string_id, string memory _imgHash) public {
-        require(bytes(_string_id).length > 0, "Identification should not be empty");
-        require(fundraisingExist[_string_id] == false, "There is already a fund raising with that identification");
-        
-        //Increment fundraisings id
-        fundRaisingsCount++;
-
-        //Add fundraising to contract
-        fundraisingsList[fundRaisingsCount] = _string_id;
-        FundRaising memory new_fundraising = FundRaising(fundRaisingsCount,_string_id,_imgHash,0,0,0,true,payable(msg.sender));
-        fundraisings[_string_id] = new_fundraising;
-        fundraisingExist[_string_id] = true;
-        //Trigger event
-        emit FundRaisingModified(new_fundraising);
-    }
-
-    function openFundRaising(string memory _string_id) public {
-        require(bytes(_string_id).length > 0, "Identification should not be empty");
-        require(fundraisings[_string_id].author == msg.sender,"Only creator can open a fundraising");
-        require(!fundraisings[_string_id].isOpen,"Can't open an already open fundraising");
-
-        fundraisings[_string_id].isOpen = true;
-        emit FundRaisingModified(fundraisings[_string_id]);
-    }
-
-    function closeFundRaising(string memory _string_id) public {
-        require(bytes(_string_id).length > 0, "Identification should not be empty");
-        require(fundraisings[_string_id].author == msg.sender,"Only creator can close a fundraising");
-        require(fundraisings[_string_id].isOpen,"Can't close an already closed fundraising");
-
-        fundraisings[_string_id].isOpen = false;
-        emit FundRaisingModified(fundraisings[_string_id]);
-    }
-
-    function donate(string memory _string_id, string memory _message) public payable {
-        //Donate funds in payable
-        //Update the funding
-        FundRaising memory donation_fr = fundraisings[_string_id];
-        require(donation_fr.isOpen);
-
-        donation_fr.donorCount++;
-        donation_fr.totalAmount += msg.value;
-        donation_fr.amountToRetrieve += msg.value;
-
-        fundraisings[_string_id] = donation_fr;
-
-        //Get donation in donors list
-        fundraisingsdonations[_string_id][donation_fr.donorCount] = Donation(msg.value, _message);
-
-        emit FundRaisingModified(donation_fr);
-        
-    }
-
-    //Given the name of a fundraising (created by the account that queries,
-    //it withdraws the pending tokens stored in the contract. Balances are updated
-    function withdrawFunds(string memory _string_id) public {
-        require(bytes(_string_id).length > 0, "Identification should not be empty");
-        require(msg.sender == fundraisings[_string_id].author, "Only the author can retrieve funds");
-        require(fundraisings[_string_id].amountToRetrieve > 0 ,"There's no funds to retrieve");
-
-        uint256 amount = fundraisings[_string_id].amountToRetrieve;
-
-        address payable owner = payable(msg.sender);
-
-        owner.transfer(amount); 
-        fundraisings[_string_id].amountToRetrieve = 0;
-
-        emit FundRaisingModified(fundraisings[_string_id]);
-    }
-
-    //Getters
-
-    /*
     //STRUCTS
     struct FundRaising {
         address creator;
@@ -121,7 +12,7 @@ contract CryptoBridge {
         string img;
         mapping(uint256 => Donor) donors;
     }
-    
+
     struct Donor {
         uint256 donorId;
         uint256 amount;
@@ -142,16 +33,16 @@ contract CryptoBridge {
     mapping(string => bool) private fundraisingExist;
 
     //Events
-    event FundRaisingCreated(string identification, string imgpath);
+    event FundRaisingCreated(string identification);
     event DonationReceived(uint256 indexed donorId, string identification, uint256 amount, string message);
     event FundsRetrieved(address indexed retriever, string identification, uint256 amount);
-  
+
     //Functions
     //Creates a fund raising associate to caster, is open by default.
     function createFundRaising(string memory identification, string memory imgpath) public {
         require(bytes(identification).length > 0, "Identification should not be empty");
         require(fundraisingExist[identification] == false, "There is already a fund raising with that identification");
-        
+
         userFundraisings[msg.sender].fundraisingCount ++;
         uint256 fundraisingCount = userFundraisings[msg.sender].fundraisingCount;
         userFundraisings[msg.sender].fundraisings[fundraisingCount] = identification;
@@ -167,7 +58,7 @@ contract CryptoBridge {
         fundRaisingsList[amountoffundraisings] = identification;
         amountoffundraisings++;
 
-        emit FundRaisingCreated(identification,imgpath);
+        emit FundRaisingCreated(identification);
     }
 
     //Opens the fund raising to enable it to receive donations
@@ -204,7 +95,7 @@ contract CryptoBridge {
 
         emit DonationReceived(donorId, identification, msg.value, message);
     }
-    
+
     //Returns the amount of fund raisings stored in the contract.
     function getAmountOfFundRaisings() public view returns (uint256) {
         return (amountoffundraisings);
@@ -222,13 +113,6 @@ contract CryptoBridge {
         return (userFundraisings[msg.sender].fundraisings[fundraiseid], fundraisings[userFundraisings[msg.sender].fundraisings[fundraiseid]].amountToRetrieve);
     }
 
-    //From 1 to the amount of all fund raisings, 
-    //returns the name of the fundraise and the amount of funds to retrieve
-    function getNameOfFundRaise(uint256 fundraiseid) public view returns (string memory) {
-        require(fundraiseid > 0, "Fund raise id should not be empty");
-        return (fundRaisingsList[fundraiseid]);
-    }
-
     //given the name of any fund raising, it returns the amount of donors, 
     //the total amount of tokens donated and if it's open
     function getFundRaising(string memory identification) public view returns (uint256, uint256, bool) {
@@ -240,11 +124,11 @@ contract CryptoBridge {
     //Given a fund raising and a number of donor, it returns the amount donated and the message left
     function getDonor(string memory identification, uint256 donorId) public view returns (uint256, string memory, uint256) {
         require(bytes(identification).length > 0, "Identification should not be empty");
-        
+
         FundRaising storage fundraising = fundraisings[identification];
         return (fundraising.donors[donorId].donorId, fundraising.donors[donorId].message, fundraising.donors[donorId].amount);
     }
-    
+
     //Given the name of a fundraising (created by the account that queries,
     //it withdraws the pending tokens stored in the contract. Balances are updated
     function withdrawFunds(string memory identification) public {
@@ -254,15 +138,9 @@ contract CryptoBridge {
 
         uint256 amount = fundraisings[identification].amountToRetrieve;
 
-        address payable owner = msg.sender;
-
-        owner.transfer(amount); 
+        payable(msg.sender).transfer(amount); 
         fundraisings[identification].amountToRetrieve = 0;
 
-        emit FundsRetrieved(msg.sender, identification, amount);
-
-        
+        emit FundsRetrieved(msg.sender, identification, amount);       
     }
-    */
 }
-
--- test/test.js
+++ test/test.js
@@ -1,255 +1,196 @@
 const { assert } = require('chai');
 
-const CryptoBridge = artifacts.require('./CryptoBridge.sol');
-
 require('chai')
   .use(require('chai-as-promised'))
   .should();
 
-contract('CryptoBridge', ([deployer, author, donator]) => {
-  let cryptobridge;
+// Chai and Mocha Tests (with revert cases)
 
-  before(async () => {
-    cryptobridge = await CryptoBridge.deployed();
+const { expect } = require("chai");
+
+// Test file using Mocha and Chai with Truffle
+
+const CryptoBridge = artifacts.require("CryptoBridge");
+
+contract("CryptoBridge Smart Contract", function (accounts) {
+  let cryptoBridge;
+  const creator = accounts[0];
+
+  beforeEach(async function () {
+    cryptoBridge = await CryptoBridge.new();
+  });
+  
+  describe('Creation', async function (){
+    it('Should create a new fund raising', async function() {
+      const identification = 'TestFund';
+      const imgpath = 'test-image.jpg';
+
+      await cryptoBridge.createFundRaising(identification, imgpath);
+      const fundraisingInfo = await cryptoBridge.getFundRaising(identification);
+
+      assert.equal(fundraisingInfo[0], 0, 'totalAmount should be 0 initially');
+      assert.equal(fundraisingInfo[1], 0, 'donorCount should be 0 initially');
+      assert.equal(fundraisingInfo[2], true, 'isOpen should be true initially');
+    });
+   
+    it('Should revert when creating a fund raising with an empty identification', async function() {
+      const identification = '';
+      const imgpath = 'test-image.jpg';
+
+      try {
+        await cryptoBridge.createFundRaising(identification, imgpath);
+        assert.fail(
+          'Creating fund raising with empty identification should revert'
+        );
+      } catch (error) {
+        assert.include(error.message, 'Identification should not be empty');
+      }
+    });
   });
 
-  describe('deployment', async () => {
-    it('deploys successfully', async () => {
-      const address = await cryptobridge.address;
-      assert.notEqual(address, 0x0);
-      assert.notEqual(address, '');
-      assert.notEqual(address, null);
-      assert.notEqual(address, undefined);
+  describe('Donate', async function (){
+    it('Should donate to a fund raising', async function() {
+      const identification = 'TestFund';
+      const message = 'Thank you for your support!';
+      const donationAmount = web3.utils.toWei('1', 'ether');
+
+      await cryptoBridge.createFundRaising(identification, 'test-image.jpg');
+      await cryptoBridge.donate(identification, message, {
+        value: donationAmount,
+      });
+
+      const fundraisingInfo = await cryptoBridge.getFundRaising(identification);
+      const donorInfo = await cryptoBridge.getDonor(identification, 1);
+
+      assert.equal(
+        fundraisingInfo[0],
+        donationAmount,
+        'totalAmount should be updated'
+      );
+      assert.equal(fundraisingInfo[1], 1, 'donorCount should be updated');
+      assert.equal(donorInfo[0], 1, 'donorId should be 1');
+      assert.equal(donorInfo[1], message, 'message should match');
+      assert.equal(donorInfo[2], donationAmount, 'donation amount should match');
     });
 
-    it('has a name',async () => {
-      const name = await cryptobridge.name()
-      assert.equal(name,'CryptoBridge');
+    it('Should revert when attempting to donate to a closed fund raising', async function() {
+      const identification = 'TestFund';
+      const message = 'Thank you for your support!';
+      const donationAmount = web3.utils.toWei('1', 'ether');
+
+      await cryptoBridge.createFundRaising(identification, 'test-image.jpg');
+      await cryptoBridge.closeFundRaising(identification);
+
+      try {
+        await cryptoBridge.donate(identification, message, {
+          value: donationAmount,
+        });
+        assert.fail('Donating to closed fund raising should revert');
+      } catch (error) {
+        assert.include(error.message, 'Fundraising is closed');
+      }
     });
   });
 
-  describe('fundraisings', async()=>{
-    let result, fundRaisingsCount;
-    const hash = 'hash123';
-    const string_id = 'firstfr';
+  describe('Open/Close', async function (){
+    it("Should open and close when required by the creator", async function () {
+      const identification = "TestFund";
+      await cryptoBridge.createFundRaising(identification, "test-image.jpg",{from: creator});
+      try {
+        await cryptoBridge.openFundRaising(identification, { from: creator });
+        assert.fail('Fund raising already open');
+      } catch (error) {
+        assert.include(error.message, 'Fund raising already open');
+      }
 
-    before(async () => {
-      result = await cryptobridge.createFundRaising(string_id, hash, {
-        from: author,
-      });
-      fundRaisingsCount = await cryptobridge.fundRaisingsCount();
+      await cryptoBridge.closeFundRaising(identification, { from: creator });
+      let fundraisingInfo = await cryptoBridge.getFundRaising(identification);
+      assert.equal(fundraisingInfo[2], false, 'fund raising should be closed');
+
+      try {
+        await cryptoBridge.closeFundRaising(identification, { from: creator });
+        assert.fail('Fund raising already closed');
+      } catch (error) {
+        assert.include(error.message, 'Fund raising already closed');
+      }
+
+      await cryptoBridge.openFundRaising(identification, { from: creator });
+      fundraisingInfo = await cryptoBridge.getFundRaising(identification);
+      assert.equal(fundraisingInfo[2], true, 'fund raising should be opened');
     });
 
-    it('creates fundraising', async()=>{  
-      //Success
-      assert.equal(fundRaisingsCount, 1); 
-      const event = result.logs[0].args.fundraising;
-      console.log(event.id);
+    it("Should revert when attempting to open a fund raising by a non-creator", async function () {
+      const identification = "TestFund";
+      await cryptoBridge.createFundRaising(identification, 'test-image.jpg');
+      await cryptoBridge.closeFundRaising(identification, { from: creator });
       
-      assert.equal(parseInt(event.id), fundRaisingsCount.toNumber(),'id is correct');
-      assert.equal(event.string_id, string_id, 'string id is correct');
-      assert.equal(event.imghash,hash,'image hash is correct');
-      assert.equal(event.totalAmount, '0','total amount is correct');
-      assert.equal(event.amountToRetrieve, '0','amount to retrieve is correct');
-      assert.equal(event.donorCount, '0','donor count is correct');
-      assert.equal(event.isOpen, true,'open state is correct');
-      assert.equal(event.author, author, 'author is correct');
+      const nonCreator = accounts[1];
+      try {
+        await cryptoBridge.openFundRaising(identification, { from: nonCreator });
+        assert.fail("Non-creator opening fund raising should revert");
+      } catch (error) {
+        assert.include(error.message, "Only the creator can open a fund raising");
+      }
+    });
 
-      await cryptobridge.createFundRaising(string_id, hash, {
-        from: author,
-      }).should.be.rejected; //Same string_id
-    })
+    it("Should revert when attempting to close a fund raising by a non-creator", async function () {
+      const identification = "TestFund";
+      await cryptoBridge.createFundRaising(identification, "test-image.jpg");
 
-    it('can open/close fundraising', async () => {
-      let result_openclose;
+      const nonCreator = accounts[1];
+      try {
+        await cryptoBridge.closeFundRaising(identification, { from: nonCreator });
+        assert.fail("Non-creator closing fund raising should revert");
+      } catch (error) {
+        assert.include(error.message, "Only the creator can close a fund raising");
+      }
+    });
+  });
+  
+  describe('Retrieve', async function() {
+    it('Should retrieve funds from a fund raising', async function() {
+      const identification = 'TestFund';
+      const withdrawalAmount = web3.utils.toWei('1', 'ether');
 
-      result_openclose = await cryptobridge.closeFundRaising(string_id, {
-        from: author,
-      });
-      
-      assert.equal(
-        result_openclose.logs[0].args.fundraising.isOpen,
-        false,
-        'fundraising closes ok'
-      );
-
-      await cryptobridge.closeFundRaising(hash, '', { from: author })
-        .should.be.rejected; //Already closed
-      
-      result_openclose = await cryptobridge.openFundRaising(string_id, {
-        from: author,
-      });
-      assert.equal(
-        result_openclose.logs[0].args.fundraising.isOpen,
-        true,
-        'fundraising opens ok'
-      );
-      
-      await cryptobridge.openFundRaising(hash, '', { from: author })
-        .should.be.rejected; //Already open
-      
-    
-    })
-
-    it('receives donations', async () => {
-      let result_donation;
-      const message = 'Test donation!';
-      const donation_value = web3.utils.toWei('1', 'Ether');
-      const web3_donation_value = new web3.utils.BN(donation_value);
-
-      let oldDonatorBalance;
-      oldDonatorBalance = await web3.eth.getBalance(donator);
-      oldDonatorBalance = new web3.utils.BN(oldDonatorBalance);
-
-      result_donation = await cryptobridge.donate(string_id, message, {
-        from: donator,
-        value: donation_value
+      await cryptoBridge.createFundRaising(identification, 'test-image.jpg');
+      await cryptoBridge.donate(identification, 'Thank you for your support!', {
+        value: withdrawalAmount,
       });
 
-      let newDonatorBalance;
-      newDonatorBalance = await web3.eth.getBalance(donator);
-      newDonatorBalance = new web3.utils.BN(newDonatorBalance);
-
-      assert.equal(   //Gas price may vary. As I'm transfering 1ETH just checking the first values
-        newDonatorBalance.toString().slice(0, 3),
-        oldDonatorBalance
-          .sub(web3_donation_value)
-          .toString()
-          .slice(0, 3),
-        'fundraising closes ok'
+      const initialBalance = await web3.eth.getBalance(creator);
+      await cryptoBridge.withdrawFunds(identification);
+      const finalBalance = await web3.eth.getBalance(creator);
+      
+      assert.isAtMost(
+        parseInt(finalBalance),
+        parseInt(initialBalance) + parseInt(withdrawalAmount),
+        'Balance should be increased by withdrawal amount'
       );
 
-      assert.equal(
-        parseInt(result_donation.logs[0].args.fundraising.donorCount),
-        1,
-        'fundraising closes ok'
+      assert.isAbove(
+        parseInt(finalBalance),
+        parseInt(initialBalance),
+        'Balance should be increased by withdrawal amount'
       );
+    });
 
-      assert.equal(
-        result_donation.logs[0].args.fundraising.totalAmount,
-        donation_value,
-        'fundraising closes ok'
-      );
+    it('Should revert when attempting to withdraw funds by a non-creator', async function() {
+      const identification = 'TestFund';
+      const withdrawalAmount = web3.utils.toWei('1', 'ether');
 
-      assert.equal(
-        result_donation.logs[0].args.fundraising.amountToRetrieve,
-        donation_value,
-        'fundraising closes ok'
-      );
-    })
-
-    it('withdraws funds', async () => {
-      let result_withdrawal;
-      const donation_value = web3.utils.toWei('1', 'Ether');
-      const web3_donation_value = new web3.utils.BN(donation_value);
-
-      let oldAuthorBalance;
-      oldAuthorBalance = await web3.eth.getBalance(author);
-      oldAuthorBalance = new web3.utils.BN(oldAuthorBalance);
-
-      result_withdrawal = await cryptobridge.withdrawFunds(string_id, {
-        from: author,
+      await cryptoBridge.createFundRaising(identification, 'test-image.jpg');
+      await cryptoBridge.donate(identification, 'Thank you for your support!', {
+        value: withdrawalAmount,
       });
 
-      let newAuthorBalance;
-      newAuthorBalance = await web3.eth.getBalance(author);
-      newAuthorBalance = new web3.utils.BN(newAuthorBalance);
-
-      assert.equal(
-        //Gas price may vary. As I'm transfering 1ETH just checking the first values
-        newAuthorBalance.toString().slice(0, 3),
-        oldAuthorBalance
-          .add(web3_donation_value)
-          .toString()
-          .slice(0, 3),
-        'fundraising closes ok'
-      );
-
-      assert.equal(
-        parseInt(result_withdrawal.logs[0].args.fundraising.amountToRetrieve),
-        0,
-        'amount to retrieve updates ok'
-      );
-
-    })
-    
-  })
-  // describe('funding', async () => {
-  //     let F_ID = 'testing_funding';
-  //     var img_path = 'abc_123';
-
-  //     let result;
-  //     let fundings_count;
-
-  //     before(async () => {
-  //         result = await cryptobridge.createFundRaising(F_ID,img_path, {
-  //             from: author,
-  //         });
-
-  //         fundings_count = await cryptobridge.getAmountOfFundRaisings(); 
-  //         console.log(fundings_count)
-  //     });
-
-  //     it('creates funding', async () => {
-  //       //SUCCESS
-  //       console.log(event);
-  //       const image = await event.imgpath;
-
-  //       assert.equal(fundings_count, 1);
-  //       const event = result.logs[0].args; // Puedo ver el result porque existe el event
-  //       assert.equal(event.identification, F_ID, 'id is correct');//VER COMO HACER ESTO
-  //       assert.equal(image, img_path, 'hash is correct');
-  //       assert.equal(event.totalAmount, '0', 'donations amount is correct');
-  //       assert.equal(event.creator, author, 'author is correct');
-
-  //       //FAILURE
-  //       await decentragram.createFundRaising(F_ID, '', { from: author })
-  //           .should.be.rejected; //it already exists
-  //     })
-
-  //     it('donates funds', async () => {
-  //       let oldAuthorBalance;
-  //       oldAuthorBalance = await web3.eth.getBalance(author);
-  //       oldAuthorBalance = new web3.utils.BN(oldAuthorBalance);
-
-  //       result = await cryptobridge.donate(F_ID,'First donation', {
-  //           from: tipper,
-  //           value: web3.utils.toWei('1', 'Ether'), 
-  //       });
-
-  //       // SUCCESS
-  //       const event = result.logs[0].args;
-        
-  //       assert.equal(
-  //           event.amountToRetrieve,
-  //           '1000000000000000000',
-  //           'tip amount is correct'
-  //       );
-                
-  //       // FAILURE: Tries to tip a image that does not exist
-  //       await cryptobridge.donate('NO FUNCIONA','First donation', {
-  //           from: tipper,
-  //           value: web3.utils.toWei('1', 'Ether'), 
-  //       }).should.be.rejected;
-  //     })
-
-  //     it('withdraws fuznds', async () => {
-  //           // Check that author received funds
-  //       let newAuthorBalance;
-  //       newAuthorBalance = await web3.eth.getBalance(author);
-  //       newAuthorBalance = new web3.utils.BN(newAuthorBalance);
-
-  //       let tipImageOwner;
-  //       tipImageOwner = web3.utils.toWei('1', 'Ether');
-  //       tipImageOwner = new web3.utils.BN(tipImageOwner);
-
-  //       const expectedBalance = oldAuthorBalance.add(tipImageOwner);
-
-  //       assert.equal(newAuthorBalance.toString(), expectedBalance.toString());
-
-  //     })
-
-
-  // })
+      const nonCreator = accounts[1];
+      try {
+        await cryptoBridge.withdrawFunds(identification, { from: nonCreator });
+        assert.fail('Non-creator withdrawing funds should revert');
+      } catch (error) {
+        assert.include(error.message, 'Only the creator can retrieve funds');
+      }
+    });
+  });
 
 });
